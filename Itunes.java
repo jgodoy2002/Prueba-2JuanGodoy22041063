8db99a82f1d725372d61d38d18c4a2a14d2b5401
Juan <jgodoy@gmail.com>
2022-03-21 08:54:19 -0600

    Prueba de Commit 2

--- Itunes.java
+++ Itunes.java
@@ -4,43 +4,66 @@
  * and open the template in the editor.
  */
 package Prueba;
+
 import java.io.*;
 import java.util.*;
+
 /**
  *
  * @author jcgof
  */
 public class Itunes {
-    RandomAccessFile codigos,canciones,descargas;
+
+    RandomAccessFile codigos, canciones, descargas;
 
     public Itunes() {
-        
-        
-        
-        
-        
+        try {
+            codigos = new RandomAccessFile("codigos.itn", "rw");
+            //int codigo disponible para una nueva cancion
+            //int codigo disponible para un nuevo donwload
+            codigos.writeInt(1);
+            codigos.writeInt(1);
+            canciones = new RandomAccessFile("songs.itn", "rw");
+            //int codigo de la cancion
+            //String nombre cancion
+            //String nombre cantante
+            //double precio para descargar
+            //double suma de estrellas totales
+            //int cantidad de reviews
+            descargas = new RandomAccessFile("downloads.itn", "rw");
+            //int codigo del download
+            //Calendar fecha al momento de bajar
+            //int codigo de la cancion 
+            //String nombre cliente
+            //double precio
+        } catch (IOException e) {
+            e.getMessage();
+        }
+
     }
-    
-    private int getCodigo(long offset){
-        
+
+    private long getCodigo(long offset) {
+        if(offset == 0){
+            
+        } else if(offset ==4){
+            
+        }
     }
-    
-    public void addSong(String nombre, String cantante, double precio){
-        
+
+    public void addSong(String nombre, String cantante, double precio) {
+
     }
-    
-    public void downloadSong(int codeSong, String cliente){
-        
+
+    public void downloadSong(int codeSong, String cliente) {
+
     }
-    
-    public String songs(String txtFile){
-        
+
+    public String songs(String txtFile) {
+
     }
-    
-    public void infoSong(int codeSong){
-        
+
+    public void infoSong(int codeSong) {
+
     }
-    
-    
-    
+
 }
